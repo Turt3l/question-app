390fa5c6b1eb445f20c9b13cf211dfad261bb555
Alberts <alberts@bright.lv>
2022-07-12 13:13:53 +0300

    final commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,25 @@
 <template>
-<div class="container" >
+<div class="test-container container-fluid d-flex justify-content-center text-center">
     <div class="starting-screen" v-if="currentForm == 0">
         <h1>Epic form</h1>
-        <button id="startButton" class="startButton" @click="startForm">start here</button>
+        <button id="startButton" class="startButton btn btn-outline-danger" @click="startForm">Start here</button>
     </div>
     <h1 v-if="currentForm <= 5 && currentForm > 0">{{ currentForm }} / 5</h1>
     <form id="first-form" v-if="currentForm < 6 && currentForm > 0">
         <div class="first-col">
             <div v-for="question, id in questions" id="for-block" v-show="id == currentForm">
-                <p id="question">{{question.name}}</p>
+                <p id="question"><b>{{question.name}}</b></p>
                 <template id="possible-answers" v-for="answer, key in question.answers">
-                    <input ref="radiod" type="radio" :value="key" v-model="pickedA">
+                    <input ref="radiod" type="radio" :value="key" v-model="pickedA" style="color: red;">
                     <label id="answer">{{answer}}</label>
-                    <a>{{pickedA}}</a>
                 </template>
             </div>
         </div>
-        <button type="button" class="submit" id="submit" @click="getInput">submit</button>
+        <button type="button" class="submit btn btn-outline-danger" id="submit" @click="getInput">Submit</button>
     </form>
     <div class="screen" v-if="currentForm > 5">
         <h1>Most pressed answer</h1>
-        <p id="mostPressed">{{ num }}</p>
+        <p id="mostPressed" style="font-size: 20px;"><b>{{ num.toUpperCase() }}</b></p>
     </div>
 </div>
 </template>
@@ -30,9 +29,7 @@
 export default {
     data() {
         return{
-            checkedValues: [],
             currentForm: 0,
-            checked: false,
             pickedA: '',
             a: 0,
             b: 0,
@@ -87,13 +84,10 @@ export default {
             //deklarē mainīgos(numbers priekš daudzuma cik tickoti)
             let numbers = {a : this.a, b : this.b, c : this.c}
             //klausās inputus kuri ir atzīmēti
-            console.log(this.pickedA)
             //seto checked uz false, lai tas nepaliek true
-            this.checked = false
             // ja nav neviens chekots, tad tas iziet no funkcijas
             if (this.pickedA == "") return false;
             // for loops prieks atbilzu skaitisanas
-            this.checkedValues.push(this.$refs.radiod.pickedA)
             this.pickedA == ""
             if (this.pickedA == "A"){
                 this.a++
@@ -109,8 +103,6 @@ export default {
             if (this.checked){
                 this.currentForm += 1
             }
-            console.log(this.checkedValues)
-            console.log(this.a, this.b, this.c)
             // kalkule lielako skaitu atbilzu burtu
             if (this.currentForm == 6){
                 this.num = Object.keys(numbers).reduce((a, b) => numbers[a] > numbers[b] ? a : b)
@@ -127,52 +119,16 @@ export default {
 </script>
 
 <style>
-    #mostPressed{
-        text-transform: uppercase;
-        font-size: 50px;
+    body {
+        background-color: red;
     }
-    .container{
-        width: 70vw;
-        height: 600px;
+    .test-container{
         background-color: white;
-        text-align: center;
+        width: 30%;
         border-radius: 25px;
-        padding-top: 100px;
+        margin-top: 20%;
     }
-    body{
-        font-family: "Haas Grot Text R Web", "Helvetica Neue", Helvetica, Arial, sans-serif;
-        background-image: radial-gradient(at left top, rgb(143, 148, 251), rgb(239, 59, 54));
-        height: 100vh;
-        width: 100%;
-        display: flex;
-        align-items: center;
-        overflow: hidden;
-        font-weight: bold;
-        color: black;
-        justify-content: center;
-    }
-    h1{
-        font-size: 50px;
-    }
-    button{
-        position: relative;
-        top: 20vh;
-        width: 200px;
-        height: 50px;
-        color: white;
-        border-radius: 70px;
-        box-shadow: 0 3px 6px #00000029;
-        text-transform: uppercase;
-        font-size: 1.5rem;
-        letter-spacing: 1px;
-        transition: .4s ease;
-        border: none;
-        background: black;
-    }
-    label {
-        font-size: 30px;
-    }
-    p {
-        font-size: 40px;
+    .test-container button{
+        margin-bottom: 20px;
     }
 </style>
